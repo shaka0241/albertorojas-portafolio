1196c3049151336f1f599bd184431f726c0c0930
Alberto Rojas <alberto.rojas@aciblabs.com>
2024-09-18 10:34:44 -0500

    Add Partytown integration for improved performance; configure Google Tag Manager in MainHead component and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://albertorojas.netlify.app/',
+    integrations: [
+        partytown({
+            config: {
+                forward: ['dataLayer.push']
+            }
+        }),
+    ]
+});
