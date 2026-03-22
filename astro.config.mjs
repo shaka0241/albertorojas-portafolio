12fb3626652a01277ec7e58836d5b89e72f7d462
Alberto Rojas <shaka0241@gmail.com>
2026-03-22 01:23:10 +0100

    feat: Enable Astro prefetching and client-side routing, and update various image assets.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
+	prefetch: true,
     integrations: [
         partytown({ config: { forward: ['dataLayer.push'] } }),
       ]
